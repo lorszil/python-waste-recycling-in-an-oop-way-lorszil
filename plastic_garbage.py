6eb41d6671fb037856fa89f45f37ec9abb1b68b2
lorszil <lorszil@gmail.com>
2016-06-22 12:50:38 +0200

    it is working now

--- plastic_garbage.py
+++ plastic_garbage.py
@@ -2,4 +2,9 @@ from garbage import Garbage
 
 
 class PlasticGarbage(Garbage):
-    pass
+    def __init__(self, name, is_clean):
+        self.name = name
+        self.is_clean = is_clean
+
+    def clean(self):
+        self.is_clean = True
