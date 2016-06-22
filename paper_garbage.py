4a294712bf82dc0d0a74e212cdb1b378d2451d89
lorszil <lorszil@gmail.com>
2016-06-22 12:49:51 +0200

    it is working now

--- paper_garbage.py
+++ paper_garbage.py
@@ -2,4 +2,9 @@ from garbage import Garbage
 
 
 class PaperGarbage(Garbage):
-    pass
+    def __init__(self, name, is_squeezed):
+        self.name = name
+        self.is_squeezed = is_squeezed
+
+    def squeeze(self):
+        self.is_squeezed = True
